1361394861f9d1e500188c6b18eeaf2ac97ffa53
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-05-29 11:03:28 -0300

    feat: production configuration

--- smllr/settings.py
+++ smllr/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o9r=_&sgxo@aagjyxh7%7pa=qiktyg-$-)e5s6zefg+m26v-42'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True').lower() in ['true', '1', 'yes']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
