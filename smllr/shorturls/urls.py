f69f2d172fdbb3632b68dad71b28f3147494ad16
Gustavo Valentim <gustavosvalentim1@gmail.com>
2025-06-20 20:46:26 -0300

    fix: short url details path and input text color

--- smllr/shorturls/urls.py
+++ smllr/shorturls/urls.py
@@ -4,6 +4,6 @@ from smllr.shorturls import views
 
 urlpatterns = [
     path('<str:short_code>', views.ShortURLView.as_view(), name='shorturls_redirect'),
+    path('<str:short_code>/details', views.ShortURLDetailsView.as_view(), name='shorturls_details'),
     path('', views.ShortURLFormView.as_view(), name='shorturls_form'),
-    path('analytics/<str:short_code>', views.ShortURLDetailsView.as_view(), name='shorturls_details'),
 ]
